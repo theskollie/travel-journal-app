7d735a1099fde864bf14c98c5b5c3b72eb1d6655
Jared <wilbrahamjared@gmail.com>
2022-03-25 00:16:47 -0400

    Recent Changes

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export default function Location(props) {
-    
-    return (
-        <div className="location-container">
-        <img src={props.item.imageUrl} />
-            <div className="location-content">
-                <div className="location--row1">
-                <h3>{props.item.location}</h3>
-                <a href={props.item.googleMapsUrl}>View on Google Maps</a>
-                </div>
-            <h1>{props.item.title}</h1>
-            <p>{props.item.startDate} - {props.item.endDate}</p>
-            <p>{props.item.description}</p>
-            </div>
+  return (
+    <div className="location-container">
+      <img src={props.item.imageUrl} />
+      <div className="location-content">
+        <div className="location--row1">
+          <h3>{props.item.location}</h3>
+          <a href={props.item.googleMapsUrl}>View on Google Maps</a>
         </div>
-    )
+        <h1>{props.item.title}</h1>
+        <p className="location--date">
+          {props.item.startDate} - {props.item.endDate}
+        </p>
+        <p>{props.item.description}</p>
+      </div>
+    </div>
+  );
 }
